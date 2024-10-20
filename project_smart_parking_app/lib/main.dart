cfbb12f3753963067b2a8f3955a63f13a06d191c
congsenpai <congcoihv5@gmail.com>
2024-10-20 23:36:36 +0700

    Welcome Screen Complete

--- project_smart_parking_app/lib/main.dart
+++ project_smart_parking_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:project_smart_parking_app/screens/home_screent.dart';
+import 'package:project_smart_parking_app/screens/welcome_screens.dart';
 
 void main() {
-  runApp(const HomeScreent());
+  runApp(const SmartParkingApp());
 }
 
 
