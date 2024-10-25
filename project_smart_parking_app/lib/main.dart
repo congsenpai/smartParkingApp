d54c78c9818d4bd62f2a14d00f1f14d14f6685ef
My name is Bảo <hagiabao980@gmail.com>
2024-10-25 16:53:19 +0700

    complete homescreent

--- project_smart_parking_app/lib/main.dart
+++ project_smart_parking_app/lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:project_smart_parking_app/screens/home_screent.dart';
 import 'package:project_smart_parking_app/screens/welcome_screens.dart';
 
 void main() {
-  runApp(const SmartParkingApp());
+  runApp(const MyApp());
 }
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
 
 
 
